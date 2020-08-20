585206a52a5861f61fad07fdc7d7dc1279defb83
Zia Ur Rehman <ziaa520@gmail.com>
2020-08-20 10:43:23 +0500

    Ok

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // 'Content-Type': undefined
+    }),
+  };
+  
+  constructor(public httpClient: HttpClient) {}
 
+  register(form) {
+    let postData = {
+            "name": "Customer004",
+            "email": "customer004@email.com",
+            "tel": "0000252525"
+    }
+
+    this.httpClient.post("http://192.236.147.77:8082/workflow/oauth2/token", form.value, this.httpOptions)
+      .subscribe(data => {
+        console.log(data);
+       }, error => {
+        console.log(error);
+      });
+
+    // this.authService.register(form.value).subscribe((res) => {
+    //   this.router.navigateByUrl('home');
+    // });
+  }
 }
