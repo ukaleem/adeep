e343fa86a9a441d794ccd570ee56296847d127e1
mehakfiaz <seharfiaz001@gmail.com>
2020-08-24 12:40:54 +0500

    token-issue-fix

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  static SERVER_ADDRESS = 'http://192.236.147.77:8082';
+  static SERVER_ADDRESS = 'http://192.236.147.77:8084';
   static WORKSPACE = 'workflow/';
   public get SERVER_API() : string {
     return ConfigService.SERVER_ADDRESS + '/' + ConfigService.WORKSPACE;
