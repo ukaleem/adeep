c3982b75d8944678ec9a3a0890b94e851833491d
ukaleem540@gmail.com <ukaleem540@gmail.com>
2020-09-24 16:56:22 +0500

    added by kaleem

--- src/app/pages/cases/cases-routing.module.ts
+++ src/app/pages/cases/cases-routing.module.ts
@@ -5,8 +5,57 @@ import { CasesPage } from './cases.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: CasesPage
+    path: 'tabs',
+    component: CasesPage,
+    children: [ 
+      {
+        path: 'home',
+        loadChildren: () => import('../admin/home/home.module').then( m => m.HomePageModule)
+      },
+
+      {
+        path: 'inbox',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./all-cases/all-cases.module').then( m => m.AllCasesPageModule)
+          }
+        ]
+      },
+
+      {
+        path: 'unassigned',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./unassigned-cases/unassigned-cases.module').then(m => m.UnassignedCasesPageModule),
+          }
+        ]
+      },
+      {
+        path: 'draft',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./draft-cases/draft-cases.module').then(m => m.DraftCasesPageModule),
+          }
+        ]
+      },
+      {
+        path: 'participated',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../participants/participants.module').then(m => m.ParticipantsPageModule),
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/cases/tabs/inbox',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: 'all-cases',
@@ -15,16 +64,20 @@ const routes: Routes = [
   {
     path: 'single-page/:caseId',
     loadChildren: () => import('./single-page/single-page.module').then( m => m.SinglePagePageModule)
-  },  {
+  },
+  {
     path: 'unassigned-cases',
     loadChildren: () => import('./unassigned-cases/unassigned-cases.module').then( m => m.UnassignedCasesPageModule)
   },
   {
     path: 'draft-cases',
     loadChildren: () => import('./draft-cases/draft-cases.module').then( m => m.DraftCasesPageModule)
+  },
+  {
+    path: '',
+    redirectTo: '/cases/tabs/inbox',
+    pathMatch: 'full'
   }
-
-
 ];
 
 @NgModule({
